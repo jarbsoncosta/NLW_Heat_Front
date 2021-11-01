dfee8dba09d76b74726cd0e4b551f63831c1b0c3
jarbsoncosta <jarbsonfc@gmail.com>
2021-11-01 12:54:33 -0300

    estilizando componente MessageList

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,7 +1,44 @@
+import styles from './styles.module.scss'
+import logoImg from '../../assets/logo.svg'
 
+export function MessageList() {
+    return (
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021" />
 
-export function MessageList(){
-    return(
-        <h1>MessageList</h1>
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}> Não vejo a hora de começar esse evento, com certeza vai ser da hora</p>
+                <div className={styles.messageUser}>
+                    <div className={styles.userImage}>
+                        <img src="https://github.com/jarbsoncosta.png" alt="Jarbson Costa" />
+                    </div>
+                    <span>Jarbson Costa</span>
+                </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}> Não vejo a hora de começar esse evento, com certeza vai ser da hora</p>
+                <div className={styles.messageUser}>
+                    <div className={styles.userImage}>
+                        <img src="https://github.com/jarbsoncosta.png" alt="Jarbson Costa" />
+                    </div>
+                    <span>Jarbson Costa</span>
+                </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}> Não vejo a hora de começar esse evento, com certeza vai ser da hora</p>
+                <div className={styles.messageUser}>
+                    <div className={styles.userImage}>
+                  
+                        <img src="https://github.com/jarbsoncosta.png" alt="Jarbson Costa" />
+                       
+                    </div>
+                    <span>Jarbson Costa</span>
+                    
+                </div>
+                </li>
+            </ul>
+
+        </div>
     )
 }
